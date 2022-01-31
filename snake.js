6ba1dbb4611dd305cccfe1118be6a1a24de1d555
Michał <Micha>
2022-01-31 12:10:09 +0100

    add input module (input.js)

--- snake.js
+++ snake.js
@@ -1,3 +1,5 @@
+import { getInputdirection } from "./input.js";
+
 export const SNAKE_SPEED = 1;
 const snakeBody = [
   { x: 10, y: 11 },
@@ -8,12 +10,14 @@ const snakeBody = [
 export function update() {
   console.log("updateSnake()");
 
+  const inputDirection = getInputdirection();
+
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] };
   }
 
-  snakeBody[0].x += 1;
-  snakeBody[0].y += 0;
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
